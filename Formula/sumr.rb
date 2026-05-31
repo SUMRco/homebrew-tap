8b544eca7fbcffbb02d74578ee80ffca673b11a2
Ivan Garzon <icgarzon@servicios.in>
2026-06-01 03:48:26 +1000

    fix: serve sumr formula assets from public tap release (#1)

--- Formula/sumr.rb
+++ Formula/sumr.rb
@@ -6,20 +6,20 @@ class Sumr < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/SUMRco/cli/releases/download/public-v0.1.1/sumr-darwin-arm64.tar.gz"
+      url "https://github.com/SUMRco/homebrew-tap/releases/download/sumr-v0.1.1/sumr-darwin-arm64.tar.gz"
       sha256 "742109631bd38eb5beef9802cbab51e733126f350a22b7d3ab1d7da5645a8f10"
     else
-      url "https://github.com/SUMRco/cli/releases/download/public-v0.1.1/sumr-darwin-x64.tar.gz"
+      url "https://github.com/SUMRco/homebrew-tap/releases/download/sumr-v0.1.1/sumr-darwin-x64.tar.gz"
       sha256 "33dfc8c4d623f02c68da0d028a3019f0323530f4ffc6a258571fde37c51bf767"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/SUMRco/cli/releases/download/public-v0.1.1/sumr-linux-arm64.tar.gz"
+      url "https://github.com/SUMRco/homebrew-tap/releases/download/sumr-v0.1.1/sumr-linux-arm64.tar.gz"
       sha256 "f743ad0121429e277060e12914011cd9a19675c1bd66ff9c4fb8a868c9bbb46f"
     else
-      url "https://github.com/SUMRco/cli/releases/download/public-v0.1.1/sumr-linux-x64.tar.gz"
+      url "https://github.com/SUMRco/homebrew-tap/releases/download/sumr-v0.1.1/sumr-linux-x64.tar.gz"
       sha256 "078741d891fd9f30145497bbaafcab5abd108f302f4091ae1e025f28063e4416"
     end
   end
